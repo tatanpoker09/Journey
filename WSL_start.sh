de70c30a356b1292a0ec8d598c9158a2753ebd84
Christian Eilers <christian@eilers.cl>
2021-12-21 00:40:09 -0300

    Added quickstart scripts

---
+++ WSL_start.sh
@@ -0,0 +1,3 @@
+source "./venv/bin/activate"
+cmd.exe /c start cmd.exe /c wsl.exe python journey_bot/bot_starter.py
+export JAVA_HOME='/usr/lib/jvm/java-11-openjdk-amd64' && ./elasticsearch-7.8.0/bin/elasticsearch
---
+++ start_mindmeld.sh
@@ -0,0 +1,2 @@
+source "venv/bin/activate"
+python -m journey_core_nlp build && python -m journey_core_nlp run
